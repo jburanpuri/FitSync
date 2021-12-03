571a2dc79267c3b39a33384d59e4d4f99ecf4fba
Janmejay Buranpuri <jburanpu@uwo.ca>
2021-12-02 19:30:41 -0500

    created few front and backend files

--- README.md
+++
@@ -1,2 +0,0 @@
-# FitSync
-FitSync Capstone
---
+++ backend/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+//run on port 3000
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+//routes
+const exercises = require('./routes/exercises');
+const users = require('./routes/users');
+
+app.use('/exercises', exercises);
+app.use('/users', users);
+
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+});
