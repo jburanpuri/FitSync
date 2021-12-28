9e9fe074711ef0c63d3875cbccd55b3dd3211ccc
Saeptum <45183693+Saeptum@users.noreply.github.com>
2021-12-28 16:41:53 -0500

    added ability to upload exercises to database with animations

--- backend/models/exerciseschema.js
+++ backend/models/exerciseschema.js
@@ -1,9 +1,11 @@
+const { default: BSON } = require('bson');
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const exerciseSchema = new Schema({
-    name: { type: String, required: true},
-    category: { type: String, required: true},
+    //name: { type: String, required: true},
+    //category: { type: String, required: true},
+    animation: { data: Buffer, contentType: String},
 },)
 
 const Exercise = mongoose.model('Exercise', exerciseSchema);
