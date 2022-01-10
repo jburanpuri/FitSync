49a6ed6bb003c7ea8f2553ffb1c5d891bb24d503
Janmejay Buranpuri <jburanpu@uwo.ca>
2022-01-10 12:45:16 -0500

    new branch, fixed an issue with server.js

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,6 @@ const cors = require('cors');
 const app = express();
 
 //run on port 3000
-app.listen(3000, () => console.log("Server is up"))
 const port = process.env.PORT || 3000;
 
 app.use(cors());
