ff69c61922378f268fd834c62d363be75134201e
Phong <phongdduong@gmail.com>
2022-01-12 06:20:03 -0500

    register screen bug fix

--- client/src/App.js
+++ client/src/App.js
@@ -9,21 +9,23 @@ import LoginScreen from "./components/screens/LoginScreen/LoginScreen";
 import RegisterScreen from "./components/screens/RegisterScreen/RegisterScreen";
 import ForgotPasswordScreen from "./components/screens/ForgotPasswordScreen/ForgotPasswordScreen";
 import ResetPasswordScreen from "./components/screens/ResetPasswordScreen/ResetPasswordScreen";
-import WelcomeScreen from "./components/screens/WelcomeScreen/WelcomeScreen";
+import LogoutScreen from "./components/screens/LogoutScreen/LogoutScreen";
+import HomeScreen from "./components/screens/HomeScreen/HomeScreen";
 
 const App = () => {
   return (
     <Router>
       <div class="topnav">
-        <a href="active" href="#home">Home</a>
-        <a href="/">Create Workout</a>
+        <a href="/">Home</a>
+        <a href="/exercise">Create Workout</a>
         <a href="/login">Login</a>
         <a href="/logout">Logout</a>
       </div>
       <div className="app">
         <Routes>
-          <Route exact path="/" element={<PrivateScreen />} />
-          <Route exact path="/logout" element={<WelcomeScreen />} />
+          <Route exact path="/" element={<HomeScreen />} />
+          <Route exact path="/exercise" element={<PrivateScreen />} />
+          <Route exact path="/logout" element={<LogoutScreen />} />
           <Route exact path="/login" element={<LoginScreen />} />
           <Route exact path="/register" element={<RegisterScreen />} />
           <Route
---
+++ client/src/components/screens/HomeScreen/HomeScreen.js
@@ -0,0 +1,11 @@
+import "./HomeScreen.css";
+
+const HomeScreen = ({ }) => {
+    return (
+        <div>This is the homepage
+        </div>
+    );
+
+};
+
+export default HomeScreen;
