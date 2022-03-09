56a0b0fe4ab7989a96e3883fa69896ec5c82d3ed
Phong Duong <pduong5@uwo.ca>
2022-03-09 07:21:03 -0500

    bug fixes

--- backend/server.js
+++ backend/server.js
@@ -3,9 +3,12 @@ const connectDB = require('./config/db');
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const fs = require('fs')
+const multer = require('multer')
 const app = express();
 const errorHandler = require("./middleware/error");
 const bodyparser = require("body-parser");
+const nodemailer = require('nodemailer')
 
 connectDB();
 
@@ -13,7 +16,6 @@ app.use(cors());
 app.use(express.json());
 
 //test
-app.use(bodyparser.json());
 
 const exercisesRouter = require('./routes/exercises')
 const workoutsRouter = require('./routes/workouts')
@@ -21,6 +23,7 @@ const workoutsRouter = require('./routes/workouts')
 
 app.use('/exercises', exercisesRouter)
 app.use('/workouts', workoutsRouter)
+
 
 app.get("/", (req, res, next) => {
     res.send("Api running");
